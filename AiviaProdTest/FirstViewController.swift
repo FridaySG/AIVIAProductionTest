c7138b6ccea2d50bd25509a93885e3d91936f1cd
Adam Farag <adam.farag@hotmail.com>
2020-01-19 16:14:52 -0700

    Initial Commit

---
+++ AiviaProdTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AiviaProdTest
+//
+//  Created by Adam on 2020-01-19.
+//  Copyright © 2020 Adam. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AiviaProdTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AiviaProdTest
+//
+//  Created by Adam on 2020-01-19.
+//  Copyright © 2020 Adam. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
